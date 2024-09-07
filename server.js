2662de6f01905d68205382feab0172b65b79cb72
deepak4a <87273982+deepak4a@users.noreply.github.com>
2024-09-07 23:49:10 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,8 +4,8 @@ const port = 80
 
 const server = http.createServer((request, response) => {
   response.writeHead(200, {'Content-Type': 'text/plain'})
-  response.write('Hello World\n')
-  response.write('Hello World2\n')
+  response.write('UAT Hello World\n')
+  response.write('UAT Hello World2\n')
   response.end('Version: ' + process.env.NODE_VERSION + '\n')
 })
 
